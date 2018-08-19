ea09d3d81f2078ba29e71e18867e39f97259a899
Alena Yevdokimova <alyona.y3@gmail.com>
2018-08-19 15:20:47 -0400

    fixed closing bracket bug in testing suits

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,18 +55,20 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+         /* test to check that initially menu is hidden
          */
+        it('is hidden', function() {
+            const body = document.querySelector('body');
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+        });
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-    }
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
@@ -76,7 +78,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-    }
+    });
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
@@ -85,5 +87,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    }
+    });
 }());
