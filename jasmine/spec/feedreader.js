957c8654a36d73cae526d0b8c6b68504a58b779d
Alena Yevdokimova <alyona.y3@gmail.com>
2018-08-19 16:27:15 -0400

    added test that checks that there is at least one initial entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,6 +84,13 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(0,done);
+         });
+         it('runs and completes loadFeed', function(){
+            const feed= document.querySelector('.feed');
+            expect(feed.children.length >0).toBe(true);
+         });
     });
 
 
