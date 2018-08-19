29bab3d41af5ed684d55bb7a11ec690a0b672f31
Alena Yevdokimova <alyona.y3@gmail.com>
2018-08-19 16:49:52 -0400

    added test that checks if content updates when new entry is added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,11 +78,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* test that ensures when the loadFeed has at least one initial entry
          */
          beforeEach(function(done){
             loadFeed(0,done);
@@ -96,9 +92,25 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+        const feed = document.querySelector('.feed');
+        const firstFeed=[];
+
+         /* test that ensures that content changes when loadFeed function is used
          */
+         beforeEach(function(done){
+            loadFeed(0);
+            Array.from(feed.children).forEach(function(entry){
+                firstFeed.push(entry.innerText);
+            });
+            loadFeed(1,done);
+
+         });
+         
+         it('content changes', function(){
+            Array.from(feed.children).forEach(function(entry,index){
+                expect(entry.innerText === firstFeed[index]).toBe(false);
+            });
+         });
+
     });
 }());
